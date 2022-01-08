f1e82b56011dafc23f6b897c0ca6a46ab9479783
Pavlos Karakalidis <pavloc.kara@outlook.com>
2022-01-08 23:36:50 +0200

    feat(autoencoder): add autoencoder and mnist classification

---
+++ autoencoder/__main__.py
@@ -0,0 +1,49 @@
+import numpy as np
+import torch
+from torchvision import transforms
+from autoencoder.utils import download_mnist_dataset, visualize_batch, visualize_image, visualize_after_decode, train, evaluate, stopwatch
+from autoencoder.autoencoder import AutoEncoder
+from torch import nn
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    num_workers = 0
+    pin_memory = False
+
+    if device == "cuda":
+        num_workers = 4
+        pin_memory = True
+    print(f"Will be using {device} for training and testing")
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    batch_size = 64
+    n_epochs = 30
+
+    train_set = download_mnist_dataset('mnist', True, transform=transform)
+    test_set = download_mnist_dataset('mnist', train=False, transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True, num_workers=num_workers,
+                                               pin_memory=pin_memory)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=64, shuffle=True, num_workers=num_workers,
+                                              pin_memory=pin_memory)
+    # visualizes a part of the batch
+    visualize_batch(train_loader)
+    # visualizes an image in more detail
+    visualize_image(np.squeeze((next(iter(train_loader))[0])[0].numpy()))
+    encoder = AutoEncoder()
+    distance = nn.MSELoss()
+    criterion = nn.CrossEntropyLoss()
+    mse_multiplier = 0.5
+    cel_multiplier = 0.5
+    optimizer = torch.optim.Adam(encoder.parameters(), lr=0.002)
+    images, labels = next(iter(train_loader))
+    images = images.view(images.shape[0], -1)
+    encoder.to(device)
+    multipliers: tuple = (mse_multiplier, cel_multiplier)
+    print(
+        f"Training took {stopwatch(lambda: train(model=encoder, device=device, train_loader=train_loader, distance=distance, criterion=criterion, optimizer=optimizer, epochs=n_epochs))}s")
+    print(
+        f"Evaluation took {stopwatch(lambda: evaluate(model=encoder, device=device, test_loader=test_loader, criterion=criterion))}s")
+    visualize_after_decode(model=encoder, device=device, test_loader=test_loader)
---
+++ autoencoder/autoencoder.py
@@ -0,0 +1,34 @@
+from torch import nn
+
+
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 4, kernel_size=5),
+            nn.Dropout2d(p=0.1),
+            nn.ReLU(True),
+            nn.Conv2d(4, 8, kernel_size=5),
+            nn.Dropout2d(p=0.1),
+            nn.ReLU(True),
+            nn.Flatten(start_dim=1),
+            nn.Linear(3200, 10)
+        )
+
+        self.softmax = nn.Softmax(dim=1)
+
+        self.decoder = nn.Sequential(
+            nn.Linear(10, 400),
+            nn.ReLU(True),
+            nn.Unflatten(1, (1, 20, 20)),
+            nn.Dropout2d(p=0.1),
+            nn.ConvTranspose2d(1, 10, kernel_size=5),
+            nn.ReLU(True),
+            nn.Dropout2d(p=0.1),
+            nn.ConvTranspose2d(10, 1, kernel_size=5)
+
+        )
+
+    def forward(self, x):
+        enc = self.encoder(x)
+        return enc, self.decoder(self.softmax(enc))
